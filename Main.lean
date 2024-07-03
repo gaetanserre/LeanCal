6ab883c9cd909475649f5ae0110bb9e58a095c0c
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-07-03 16:53:16 +0200

    Update placeholder variable

--- Main.lean
+++ Main.lean
@@ -7,9 +7,9 @@ import LeanCal.Date
 import LeanCal.Waybar
 
 def main (argv : List String) : IO Unit := do
-  let LeanCal_HOME := "LeanCal_HOME"
-  let fevents := s!"{LeanCal_HOME}/events.txt"
-  let fpast_events := s!"{LeanCal_HOME}/past_events.txt"
+  let LeanCal_home := "LeanCal_HOME"
+  let fevents := s!"{LeanCal_home}/events.txt"
+  let fpast_events := s!"{LeanCal_home}/past_events.txt"
   match argv with
     | [] => calendar_run fevents fpast_events
     | ["-w"] | ["--waybar"] => get_waybar_events fevents >>= IO.println
